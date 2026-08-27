fc1670b4e2a97cdbfa26f8aaaa7d8c797dea96ef
Tanishka <tanishkayadav901@gmail.com>
2026-08-27 14:37:17 +0530

    Add simple interest calculation program

---
+++ simpleinterest.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,simple interest;
+printf("enter three numbers a&b&c");
+scanf("%d %d %d",&a,&b,&c);
+simple interest= a*b*c/100;
+printf("simple interest of the numbers is = %d", simple interest);
+}
